cf154a3fc5e14b1b79bcc5b4c297358d1dde49a6
antieatingactivist <antieatingactivist@gmail.com>
2023-05-12 13:07:35 -0700

    connected to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import { initializeApp } from "firebase/app";
+import { collection, addDoc, getFirestore } from "firebase/firestore"; 
+// import { getAnalytics } from "firebase/analytics";
+
 import './App.css';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjBwVlOOdgvoqrQQmN8yJn5pvbx14uJ44",
+  authDomain: "invoicing-test-23f3c.firebaseapp.com",
+  projectId: "invoicing-test-23f3c",
+  storageBucket: "invoicing-test-23f3c.appspot.com",
+  messagingSenderId: "133095410928",
+  appId: "1:133095410928:web:74a597e73eb02f65678635",
+  measurementId: "G-F1N0J6C5H3"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+async function addData() {
+  // console.log(db);
+  try {
+    const docRef = await addDoc(collection(db, "invoices"), {
+      client: "Client 1",
+      bill: 100.00
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } 
+  catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={addData}>add data</button>
     </div>
   );
 }
