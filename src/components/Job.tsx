f7f082704ed49b377ed6215e08fe431bc9dcf576
antieatingactivist <antieatingactivist@gmail.com>
2023-05-12 18:58:04 -0700

    delete works

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -12,7 +12,7 @@ export default function Job({job, methods}: Props) {
             <p>Client: {job.client}</p>
             <p>Date: {new Date(job.date).toLocaleDateString()}</p>
             <p>Bill: ${job.bill.toFixed(2)}</p>
-            {job.id}
+  
             <button onClick={() => methods.deleteData(job.id)}>Delete</button>
             <button>Edit</button>
         </div>
